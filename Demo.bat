7a0df03c4bc44f5a70a0e882030fd14dda726c34
Patrick <Patrick.Kutch@gmail.com>
2017-12-01 09:45:07 -0800

    comment in demo bat file

--- Demo.bat
+++ Demo.bat
@@ -1,1 +1,2 @@
+rem This runs FUDD using the config file specified.  Look at and modify the DemoConfig.xml file to learn the different capabilities
 python Fudd.py -i Demonstration\DemoConfig.xml -o NewSaveFile.biff 
